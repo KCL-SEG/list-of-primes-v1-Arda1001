2a24b200a8fefed1fee5fa3e8e6784e00eb3639f
Arda Ordu <ardaordu03@gmail.com>
2022-09-28 22:03:20 +0100

    Complete prime numbers task

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    number = 2
+    while count < number_of_primes:
+         prime = True
+         for i in range(2, number):
+             if number % i == 0:
+                 prime = False
+                 break
+         if prime:
+             list.append(number)
+             count += 1
+         number += 1
     return list
+print(primes(10))
